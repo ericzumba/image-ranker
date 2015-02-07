c8307fa8c1b9b33eb5f11234c7d81fb44148eb20
Roberto Marin <robertomarin@gmail.com>
2015-02-07 14:14:26 -0200

    go go go powerrangers!

--- routes/index.js
+++ routes/index.js
@@ -6,13 +6,17 @@ router.get('/', function(req, res, next) {
 
 	console.log('---->>> sort: ' + sort);
 	var sort = req.query.sort;
-	if(sort == null) {
+	if(!sort) {
 		sort = 'hash';
 	}
 
 	console.log('---->>> sort: ' + sort);
 	
 	ImageInfo.find().sort('-' + sort).exec(function(err, list) {
+		
+		if(!req.query.sort)
+			list = list.sort(function(){ return .5 - Math.random(); });
+		
 		res.render('index', {
 			list: list,
 			show: req.query.show
